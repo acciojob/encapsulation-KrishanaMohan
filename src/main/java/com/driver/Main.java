067670d555157ddeed42eb02bc08a97c82e3eea8
KrishanaMohan <ram227209@gmail.com>
2022-12-15 14:51:24 +0530

    ok

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String []args){
+      RWOnly name= new RWOnly();
+
+     // name.Myname="mohan"; //java: Myname has private access in com.driver.RWOnly
+
+        name.setMyname("Mohan");
+        System.out.println(name.getMyname());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+
+
 public class RWOnly {
-    
+    private String Myname="Krishana";
+
+    public String getMyname() {
+        return Myname;
+    }
+
+    public void setMyname(String myname) {
+        Myname = myname;
+    }
 }
